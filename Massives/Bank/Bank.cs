4186cdf90a312d1c479fd729660c46c2231523c2
emoRussiunGirl <yelizaveta.cherepanova.04@mail.ru>
2023-04-01 14:25:18 +0300

    Задание на массивы.

---
+++ Massives/Bank/Bank.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    //Банк.
+    public class Bank
+    {
+        public decimal Money { get; set; }
+
+        public Report[,] Reports { get; set; }
+
+        //Конструктор по умолчанию, для содания пустого банка.
+        public Bank()
+        {
+            Money = 0;
+            Reports = new Report[0, 0];
+        }
+
+        //Конструктор для инициализации банка деньгами и отчетами
+        public Bank(decimal money, Report[,] reports)
+        {
+            Money = money;
+            Reports = reports;
+        }
+
+        //Индексатор, отдающий очтеты по их порядковому номеру и id
+        public Report this[int index, int id]
+        {
+            get
+            {
+                return Reports[index, id];
+            }
+
+            set
+            {
+                Reports[index, id] = value;
+            }
+        }
+
+        //Янвное приведение к типу decimal, получение денег банка.
+        public static explicit operator decimal(Bank bank)
+        {
+            return bank.Money;
+        }
+
+
+    }
+    
+    //Отчет
+    public class Report
+    {
+        public int Id { get; set; }
+
+        //Поле инт
+        public int Money { get; set; }
+
+        public Report(int id, int money)
+        {
+            Id = id;
+            Money = money;
+        }
+    }
+}
---
+++ Massives/Bank/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Bank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Report[,] reports = new Report[2, 3];
+            reports[0, 0] = new Report(0, 100);
+            reports[0, 1] = new Report(1, 1000);
+            reports[0, 2] = new Report(2, 83000);
+            reports[1, 0] = new Report(3, 32000);
+            reports[1, 1] = new Report(4, 4000);
+            reports[1, 2] = new Report(5, 5000);
+
+            Bank bank = new Bank(99000000, reports);
+
+            Console.WriteLine($"{bank[0, 0].Money}");
+
+            decimal money = (decimal)bank;
+        }
+    }
+}
